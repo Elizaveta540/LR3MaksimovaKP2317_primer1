9e7ee53ffbc9c903ec9202632f4926a7253fb8be
Elizaveta540 <Lissa.maksimova@mail.ru>
2026-04-09 14:15:07 +0000

    изменение 1

--- main.py
+++ main.py
@@ -1,176 +1,286 @@
-#общая часть
-#Контейнер расчета
-from sympy import *
-k,T,C,L = symbols ('k T C L')
+# #общая часть
+# #Контейнер расчета
+# from sympy import *
+# k,T,C,L = symbols ('k T C L')
 
-#1 способ
-C_ost = 100000
-Am_lst = []
-C_ost_lst = []
-for i in range (5):
-  Am = (C-L)/T
-  C_ost -= Am.subs({C:100000,T:5, L:0})
-  Am_lst.append(round(Am.subs({C:100000,T:5, L:0}),2))
-  C_ost_lst.append(round(C_ost,2))
-print('Am_lst:', Am_lst)
-print('C_ost_lst:', C_ost_lst)
+# #1 способ
+# C_ost = 100000
+# Am_lst = []
+# C_ost_lst = []
+# for i in range (5):
+#   Am = (C-L)/T
+#   C_ost -= Am.subs({C:100000,T:5, L:0})
+#   Am_lst.append(round(Am.subs({C:100000,T:5, L:0}),2))
+#   C_ost_lst.append(round(C_ost,2))
+# print('Am_lst:', Am_lst)
+# print('C_ost_lst:', C_ost_lst)
 
-#2 способ
-Aj = 0
-C_ost = 100000
-Am_lst_2 = []
-C_ost_lst_2 = []
-for i in range (5):
-  Am = k*1/T*(C-Aj)
-  C_ost -= Am.subs({C:100000,T:5, k:2})
-  Am_lst_2.append(round(Am.subs({C:100000,T:5, k:2}),2))
-  Aj += Am
-  C_ost_lst_2.append(round(C_ost,2))
-  print('Am_lst_2:', Am_lst_2)
-  print('C_ost_lst_2:',C_ost_lst_2)
+# #2 способ
+# Aj = 0
+# C_ost = 100000
+# Am_lst_2 = []
+# C_ost_lst_2 = []
+# for i in range (5):
+#   Am = k*1/T*(C-Aj)
+#   C_ost -= Am.subs({C:100000,T:5, k:2})
+#   Am_lst_2.append(round(Am.subs({C:100000,T:5, k:2}),2))
+#   Aj += Am
+#   C_ost_lst_2.append(round(C_ost,2))
+#   print('Am_lst_2:', Am_lst_2)
+#   print('C_ost_lst_2:',C_ost_lst_2)
 
-#3 часть
-C_ost = 30000
-Am_lst = []
-C_ost_lst = []
-for i in range (8):
-    Am = (C-L)/T
-    C_ost -= Am.subs({C:30000,T:8, L:0})
-    Am_lst.append(round(Am.subs({C:30000,T:8, L:0}),2))
-    C_ost_lst.append(round(C_ost,2))
-print('Am_lst:', Am_lst)
-print('C_ost_lst:', C_ost_lst)
+# #3 часть
+# C_ost = 30000
+# Am_lst = []
+# C_ost_lst = []
+# for i in range (8):
+#     Am = (C-L)/T
+#     C_ost -= Am.subs({C:30000,T:8, L:0})
+#     Am_lst.append(round(Am.subs({C:30000,T:8, L:0}),2))
+#     C_ost_lst.append(round(C_ost,2))
+# print('Am_lst:', Am_lst)
+# print('C_ost_lst:', C_ost_lst)
 
-Aj = 0
-C_ost = 30000
-Am_lst_2 = []
-C_ost_lst_2 = []
-for i in range (8):
-  Am = k*1/T*(C-Aj)
-  C_ost -= Am.subs({C:30000,T:8, k:2})
-  Am_lst_2.append(round(Am.subs({C:30000,T:8, k:2}),2))
-  Aj += Am
-  C_ost_lst_2.append(round(C_ost,2))
-print('Am_lst_2:', Am_lst_2)
-print('C_ost_lst_2:',C_ost_lst_2)
+# Aj = 0
+# C_ost = 30000
+# Am_lst_2 = []
+# C_ost_lst_2 = []
+# for i in range (8):
+#   Am = k*1/T*(C-Aj)
+#   C_ost -= Am.subs({C:30000,T:8, k:2})
+#   Am_lst_2.append(round(Am.subs({C:30000,T:8, k:2}),2))
+#   Aj += Am
+#   C_ost_lst_2.append(round(C_ost,2))
+# print('Am_lst_2:', Am_lst_2)
+# print('C_ost_lst_2:',C_ost_lst_2)
   
-#Контейнер для табличного вывода
-import pandas as pd
-Y = range (1,6)
-table1 = list(zip(Y,Am_lst,C_ost_lst))
-table2 = list(zip(Y,Am_lst_2,C_ost_lst_2))
-tfame = pd.DataFrame(table1, columns = ['Y','Am_lst','C_ost_lst'])
-tfame2 = pd.DataFrame(table2, columns = ['Y','Am_lst_2','C_ost_lst_2'])
-print(tfame)
-print(tfame2)
+# #Контейнер для табличного вывода
+# import pandas as pd
+# Y = range (1,6)
+# table1 = list(zip(Y,Am_lst,C_ost_lst))
+# table2 = list(zip(Y,Am_lst_2,C_ost_lst_2))
+# tfame = pd.DataFrame(table1, columns = ['Y','Am_lst','C_ost_lst'])
+# tfame2 = pd.DataFrame(table2, columns = ['Y','Am_lst_2','C_ost_lst_2'])
+# print(tfame)
+# print(tfame2)
 
-#Контейнер для визуализации
-import numpy as np
-import matplotlib.pyplot as plt
-plt.plot(tfame['Y'],tfame['C_ost_lst'], label = 'Am')
-plt.savefig('chart1.png')
-plt.figure()
-plt.plot(tfame2['Y'],tfame2['C_ost_lst_2'], label = 'Am_2')
-plt.savefig('chart2.png')
+# #Контейнер для визуализации
+# import numpy as np
+# import matplotlib.pyplot as plt
+# plt.plot(tfame['Y'],tfame['C_ost_lst'], label = 'Am')
+# plt.savefig('chart1.png')
+# plt.figure()
+# plt.plot(tfame2['Y'],tfame2['C_ost_lst_2'], label = 'Am_2')
+# plt.savefig('chart2.png')
 
-vals = Am_lst
-labels = [str(x) for x in range(1,9)]
-explode = (0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1)
-fig, ax = plt.subplots()
-ax.pie(vals, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True, wedgeprops = {'lw':1, 'ls':'--', 'edgecolor':'k'}, rotatelabels = True)
-ax.axis('equal')
-plt.savefig('chart3.png')
+# vals = Am_lst
+# labels = [str(x) for x in range(1,9)]
+# explode = (0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1)
+# fig, ax = plt.subplots()
+# ax.pie(vals, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True, wedgeprops = {'lw':1, 'ls':'--', 'edgecolor':'k'}, rotatelabels = True)
+# ax.axis('equal')
+# plt.savefig('chart3.png')
 
-vals = Am_lst_2
-labels = [str(x) for x in range(1,9)]
-explode = (0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1)
-fig, ax = plt.subplots()
-ax.pie(vals, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True, wedgeprops = {'lw':1, 'ls':'--', 'edgecolor':'k'}, rotatelabels = True)
-ax.axis('equal')
-plt.savefig('chart4.png')
+# vals = Am_lst_2
+# labels = [str(x) for x in range(1,9)]
+# explode = (0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1)
+# fig, ax = plt.subplots()
+# ax.pie(vals, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True, wedgeprops = {'lw':1, 'ls':'--', 'edgecolor':'k'}, rotatelabels = True)
+# ax.axis('equal')
+# plt.savefig('chart4.png')
 
-plt.figure()
-table1 = list(zip(Y,Am_lst))
-table2 = list(zip(Y,Am_lst_2))
-tfame = pd.DataFrame(table1, columns = ['Y','Am_lst'])
-tfame2 = pd.DataFrame(table2, columns = ['Y','Am_lst_2'])
-plt.bar(tfame['Y'],tfame['Am_lst'])
-plt.savefig('chart5.png')
-plt.figure()
-plt.bar(tfame2['Y'],tfame2['Am_lst_2'])
-plt.savefig('chart6.png')
-plt.figure()
+# plt.figure()
+# table1 = list(zip(Y,Am_lst))
+# table2 = list(zip(Y,Am_lst_2))
+# tfame = pd.DataFrame(table1, columns = ['Y','Am_lst'])
+# tfame2 = pd.DataFrame(table2, columns = ['Y','Am_lst_2'])
+# plt.bar(tfame['Y'],tfame['Am_lst'])
+# plt.savefig('chart5.png')
+# plt.figure()
+# plt.bar(tfame2['Y'],tfame2['Am_lst_2'])
+# plt.savefig('chart6.png')
+# plt.figure()
 # # Индивидуальная часть задания. Вариант 7: Ост.стоимость =0; коэф.уск.=2; нач.ст. =70000; срок=8 
 # #Контейнер расчета
+# from sympy import *
+# k,T,C,L = symbols ('k T C L')
+# #1 часть
+# C_ost = 30000
+# Am_lst = []
+# C_ost_lst = []
+# for i in range (8):
+#     Am = (C-L)/T
+#     C_ost -= Am.subs({C:30000,T:7, L:0})
+#     Am_lst.append(round(Am.subs({C:30000,T:7, L:0}),2))
+#     C_ost_lst.append(round(C_ost,2))
+# print('Am_lst:', Am_lst)
+# print('C_ost_lst:', C_ost_lst)
+
+# Aj = 0
+# C_ost = 30000
+# Am_lst_2 = []
+# C_ost_lst_2 = []
+# for i in range (8):
+#   Am = k*1/T*(C-Aj)
+#   C_ost -= Am.subs({C:30000,T:7, k:2})
+#   Am_lst_2.append(round(Am.subs({C:30000,T:7, k:2}),2))
+#   Aj += Am
+#   C_ost_lst_2.append(round(C_ost,2))
+# print('Am_lst_2:', Am_lst_2)
+# print('C_ost_lst_2:',C_ost_lst_2)
+
+# #Контейнер для табличного вывода
+# import pandas as pd
+# Y = range (1,8)
+# table1 = list(zip(Y,Am_lst,C_ost_lst))
+# table2 = list(zip(Y,Am_lst_2,C_ost_lst_2))
+# tfame = pd.DataFrame(table1, columns = ['Y','Am_lst','C_ost_lst'])
+# tfame2 = pd.DataFrame(table2, columns = ['Y','Am_lst_2','C_ost_lst_2'])
+# print(tfame)
+# print(tfame2)
+
+# #Контейнер для визуализации
+# import numpy as np
+# import matplotlib.pyplot as plt
+# plt.plot(tfame['Y'],tfame['C_ost_lst'], label = 'Am')
+# plt.savefig('chart7.png')
+# plt.figure()
+# plt.plot(tfame2['Y'],tfame2['C_ost_lst_2'], label = 'Am_2')
+# plt.savefig('chart8.png')
+
+
+
+# vals = Am_lst
+# labels = [str(x) for x in range(1,8)]
+# explode = (0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1)
+# fig, ax = plt.subplots()
+# ax.pie(vals,
+# labels=labels,
+# autopct='%1.1f%%',
+# shadow=True,
+# explode=explode,
+# wedgeprops={
+# 'lw': 1,
+# 'ls': '--',
+# 'edgecolor': "k"
+# },
+# rotatelabels=True)
+# ax.axis("equal")
+# plt.savefig('chart9.png')
+
+# vals = Am_lst_2
+# labels = [str(x) for x in range(1,8)]
+# explode = (0.1,0.1,0.1,0.1,0.1,0.1,0.1)
+# fig, ax = plt.subplots()
+# ax.pie(vals, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True, wedgeprops = {'lw':1, 'ls':'--', 'edgecolor':'k'}, rotatelabels = True)
+# ax.axis('equal')
+# plt.savefig('chart10.png')
+
+# plt.figure()
+# table1 = list(zip(Y,Am_lst))
+# table2 = list(zip(Y,Am_lst_2))
+# tfame = pd.DataFrame(table1, columns = ['Y','Am_lst'])
+# tfame2 = pd.DataFrame(table2, columns = ['Y','Am_lst_2'])
+# plt.bar(tfame['Y'],tfame['Am_lst'])
+# plt.savefig('chart11.png')
+# plt.figure()
+# plt.bar(tfame2['Y'],tfame2['Am_lst_2'])
+# plt.savefig('chart12.png')
+
+
+# Индивидуальная часть задания 3 var
 from sympy import *
-k,T,C,L = symbols ('k T C L')
-#1 часть
-C_ost = 70000
+k, T, C, L = symbols('k T C L')
+#1st
+C_ost = 30000
 Am_lst = []
 C_ost_lst = []
-for i in range (8):
-    Am = (C-L)/T
-    C_ost -= Am.subs({C:70000,T:8, L:0})
-    Am_lst.append(round(Am.subs({C:70000,T:8, L:0}),2))
-    C_ost_lst.append(round(C_ost,2))
-print('Am_lst:', Am_lst)
-print('C_ost_lst:', C_ost_lst)
+for i in range(7):
+  Am = (C - L) / T
+  C_ost -= Am.subs({C: 30000, T: 7, L: 0})
+  Am_lst.append(round(Am.subs({C: 30000, T: 7, L: 0}), 2))
+  C_ost_lst.append(round(C_ost, 2))
+  print('Am_lst:', Am_lst)
+  print('C_ost_lst:', C_ost_lst)
 
+#2nd
 Aj = 0
-C_ost = 70000
+C_ost = 30000
 Am_lst_2 = []
 C_ost_lst_2 = []
-for i in range (8):
-  Am = k*1/T*(C-Aj)
-  C_ost -= Am.subs({C:70000,T:8, k:2})
-  Am_lst_2.append(round(Am.subs({C:70000,T:8, k:2}),2))
+for i in range(7):
+  Am = k * 1 / T * (C - Aj)
+  C_ost -= Am.subs({C: 30000, T: 7, k: 2})
+  Am_lst_2.append(round(Am.subs({C: 30000, T: 7, k: 2}), 2))
   Aj += Am
-  C_ost_lst_2.append(round(C_ost,2))
-print('Am_lst_2:', Am_lst_2)
-print('C_ost_lst_2:',C_ost_lst_2)
+  C_ost_lst_2.append(round(C_ost, 2))
+  print('Am_lst_2:', Am_lst_2)
+  print('C_ost_lst_2:', C_ost_lst_2)
 
-#Контейнер для табличного вывода
+#Frame
 import pandas as pd
-Y = range (1,9)
-table1 = list(zip(Y,Am_lst,C_ost_lst))
-table2 = list(zip(Y,Am_lst_2,C_ost_lst_2))
-tfame = pd.DataFrame(table1, columns = ['Y','Am_lst','C_ost_lst'])
-tfame2 = pd.DataFrame(table2, columns = ['Y','Am_lst_2','C_ost_lst_2'])
-print(tfame)
-print(tfame2)
 
-#Контейнер для визуализации
+Y = range(1, 8)
+table1 = list(zip(Y, C_ost_lst, Am_lst))
+table2 = list(zip(Y, C_ost_lst_2, Am_lst_2))
+tframe = pd.DataFrame(table1, columns=['Y', 'C_ost_lst', 'Am_lst'])
+tframe2 = pd.DataFrame(table2, columns=['Y', 'C_ost_lst_2', 'Am_lst_2'])
+print(tframe)
+print(tframe2)
+
+#visualisation
 import numpy as np
 import matplotlib.pyplot as plt
-plt.plot(tfame['Y'],tfame['C_ost_lst'], label = 'Am')
+
+plt.plot(tframe['Y'], tframe['C_ost_lst'], label='Am')
 plt.savefig('chart7.png')
 plt.figure()
-plt.plot(tfame2['Y'],tfame2['C_ost_lst_2'], label = 'Am_2')
+plt.plot(tframe2['Y'], tframe2['C_ost_lst_2'], label='Am_2')
 plt.savefig('chart8.png')
-
-vals = Am_lst
-labels = [str(x) for x in range(1,9)]
-explode = (0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1)
-fig, ax = plt.subplots()
-ax.pie(vals, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True, wedgeprops = {'lw':1, 'ls':'--', 'edgecolor':'k'}, rotatelabels = True)
-ax.axis('equal')
-plt.savefig('chart9.png')
-
-vals = Am_lst_2
-labels = [str(x) for x in range(1,9)]
-explode = (0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1)
-fig, ax = plt.subplots()
-ax.pie(vals, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True, wedgeprops = {'lw':1, 'ls':'--', 'edgecolor':'k'}, rotatelabels = True)
-ax.axis('equal')
-plt.savefig('chart10.png')
-
 plt.figure()
-table1 = list(zip(Y,Am_lst))
-table2 = list(zip(Y,Am_lst_2))
-tfame = pd.DataFrame(table1, columns = ['Y','Am_lst'])
-tfame2 = pd.DataFrame(table2, columns = ['Y','Am_lst_2'])
-plt.bar(tfame['Y'],tfame['Am_lst'])
+vals = Am_lst
+labels = [str(x) for x in range(1, 8)]
+explode = (0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1)
+fig, ax = plt.subplots()
+ax.pie(vals,
+labels=labels,
+autopct='%1.1f%%',
+shadow=True,
+explode=explode,
+wedgeprops={
+'lw': 1,
+'ls': '--',
+'edgecolor': "k"
+},
+rotatelabels=True)
+ax.axis("equal")
+plt.savefig('chart9.png')
+vals = Am_lst_2
+labels = [str(x) for x in range(1, 8)]
+explode = (0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1)
+fig, ax = plt.subplots()
+ax.pie(vals,
+labels=labels,
+autopct='%1.1f%%',
+shadow=True,
+explode=explode,
+wedgeprops={
+'lw': 1,
+'ls': '--',
+'edgecolor': "k"
+},
+rotatelabels=True)
+ax.axis("equal")
+plt.savefig('chart10.png')
+table1 = list(zip(Y, C_ost_lst, Am_lst))
+table2 = list(zip(Y, C_ost_lst_2, Am_lst_2))
+tframe = pd.DataFrame(table1, columns=['Y', 'C_ost_lst', 'Am_lst'])
+tframe2 = pd.DataFrame(table2, columns=['Y', 'C_ost_lst_2', 'Am_lst_2'])
+plt.figure()
+plt.bar(tframe['Y'], tframe['Am_lst'])
 plt.savefig('chart11.png')
 plt.figure()
-plt.bar(tfame2['Y'],tfame2['Am_lst_2'])
+plt.bar(tframe2['Y'], tframe2['Am_lst_2'])
 plt.savefig('chart12.png')
+
+
